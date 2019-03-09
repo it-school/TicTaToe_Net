d5300747b8e73c1ddadd0a9336232fbe09df6721
it-school <it@osvita.od.ua>
2019-03-09 19:04:28 +0200

    Human turn added

--- TicTaToe/Program.cs
+++ TicTaToe/Program.cs
@@ -16,33 +16,37 @@ namespace TicTaToe
 			for (int i = 0; i < n; i++)
 				for (int j = 0; j < n; j++)
 					field[i,j] = 0;
-
+            
 			ShowField(field, n);
 
 			for (int i = 0; i < n * n; i++)
 			{
 				NextMove(field, n, i % 2 + 1);
 				ShowField(field, n);
-				if (CheckVictory(field, n, i % 2 + 1))
+				if (CheckVictory(field, n, i % 2 + 1) == true)
 				{
 					Console.WriteLine("Winner is " + ((i % 2 + 1) == 1 ? "X" : "0"));
+                    Console.ReadLine();
 					return;
 				}
 			}
-
 			Console.WriteLine("Draw");
+            Console.ReadLine();
 		}
 
 		public static void ShowField(int[,] field, int n)
 		{
 			Console.WriteLine("\n\n");
-			for (int i = 0; i < n; i++)
+			Console.WriteLine("\n-------------------------");
+			
+            for (int i = 0; i < n; i++)
 			{
 				for (int j = 0; j < n; j++)
 				{
-					Console.Write("\t" + (field[i,j] == 0 ? " " : (field[i,j] == 1 ? "X" : "O")));
+					Console.Write("| \t" + (field[i,j] == 0 ? " " : (field[i,j] == 1 ? "X" : "O")));
 				}
-				Console.WriteLine();
+                Console.Write("|");
+				Console.WriteLine("\n-------------------------");
 			}
 		}
 
@@ -50,13 +54,30 @@ namespace TicTaToe
 		{
 			Random r = new Random();
 			int moveX, moveY;
-
+            /*
 			while (field[moveX = r.Next(3), moveY = r.Next(3)] != 0)
 			{
-				moveX = r.Next(3);
-				moveY = r.Next(3);
-			}
 
+            }
+*/    
+        do
+            {
+            if (m == 1)
+            {
+                moveX = r.Next(3);
+                moveY = r.Next(3);
+                }
+                else
+                {
+                    do{
+                        Console.WriteLine("Humans turn: ");
+                    moveX = Convert.ToInt32(Console.ReadLine());
+                    moveY = Convert.ToInt32(Console.ReadLine());
+                        } while ((moveX < 0 || moveX > n-1) || (moveY < 0 || moveY > n-1));
+                }
+            }while (field[moveX, moveY] != 0);
+
+    
 			field[moveX,moveY] = m;
 		}
 
@@ -64,14 +85,15 @@ namespace TicTaToe
 		{
 			int i;
 
-			for (i = 0; i < n; i++)
+			for (i = 0; i < n; i++)  //проверка по горизонтали
 			{
 				if (field[i,0] == m && field[i,1] == m && field[i,2] == m)
 				{
 					return true;
 				}
 			}
-			for (i = 0; i < n; i++)
+
+			for (i = 0; i < n; i++) // проверка по вертикали
 			{
 				if (field[0,i] == m && field[1,i] == m && field[2,i] == m)
 				{
@@ -79,27 +101,27 @@ namespace TicTaToe
 				}
 			}
 
-			for (i = 0; i < n - 1; i++)
+			for (i = 0; i < n - 1; i++)  // проверка по главной диагонали
 			{
-				if (field[i,i] == 0 || field[i,i] != field[i + 1,i + 1])
+				if (field[i, i] == 0 || field[i,i] != field[i + 1,i + 1])
 				{
 					break;
 				}
 			}
-			if (i == 2)
+			if (i == n - 1)
 			{
 				return true;
 			}
 
-			for (i = 0; i < n - 1; i++)
+			for (i = 0; i < n - 1; i++) // проверка по побочнай диагонали
 			{
-				if (field[i,i] == 0 || field[i,n - i - 1] != field[i + 1,n - 1 - i - 1])
+				if (field[i, i] == 0 || field[i, n - i - 1] != field[i + 1, n - 1 - i - 1])
 				{
 					break;
 				}
 			}
 
-			if (i == 2)
+			if (i == n-1)
 			{
 				return true;
 			}
